5cd0f696dfbdaf81c06e02245f41f6c5a126137f
SabyrrZhaslanuly <sabyrr.zhaslanuly@gmail.com>
2025-02-18 23:17:34 +0500

    8 hw

--- HW8/Task1/src/Main.java
+++ HW8/Task1/src/Main.java
@@ -1,15 +1,41 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         //#1 Создать массив из 10 целых чисел. Значения могут быть любыми
         //todo
+        int[] numbers = new int[10];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = new Random().nextInt(100);
+        }
 
         //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
         //todo
+        for (int j : numbers) {
+            System.out.print(j + " ");
+        }
+
+        System.out.println();
 
         //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
         //todo
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            System.out.print(numbers[i] + " ");
+        }
+
+        System.out.println();
 
         //#4 *** Задание особой сложности. Развернуть массив(см конец урока).
         //todo
+        int[] reversedArray = new int[numbers.length];
+        int j = 0;
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            reversedArray[j] = numbers[i];
+            j++;
+        }
+
+        for (int k : reversedArray) {
+            System.out.print(k + " ");
+        }
     }
 }
--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         //todo код программы писать тут
+        int[] peopleWeight = new int[10];
+        for (int i = 0; i < peopleWeight.length; i++) {
+            peopleWeight[i] = new Random().nextInt(40, 100);
+        }
+
+        // average weight
+        int summary = 0;
+        for (int j : peopleWeight) {
+            summary += j;
+        }
+        int averageWeight = summary / peopleWeight.length;
+        System.out.println("Средний вес всех людей равен: " + averageWeight + " кг.");
+
+        // range from 60 to 80
+        int counter = 0;
+        for (int j : peopleWeight) {
+            if (60 <= j && j <= 80) {
+                counter += 1;
+            }
+        }
+        System.out.println("Количество людей у которых вес находится в промежутке от 60 до 80 кг: " + counter);
     }
 }
